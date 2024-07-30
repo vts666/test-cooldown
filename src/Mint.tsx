b5764aa793c8b4cd1bde3ad0ad3fa5061e21586d
vts666 <118835097+vts666@users.noreply.github.com>
2024-07-30 19:57:28 +0200

    Update Mint.tsx

--- src/Mint.tsx
+++ src/Mint.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { toNano } from '@ton/ton';
 import { beginCell } from '@ton/ton';
 import { TonConnectUIProvider, useTonConnectUI, useTonWallet, useTonAddress } from '@tonconnect/ui-react';
@@ -33,7 +33,7 @@ const myTransaction = {
   validUntil: Math.floor(Date.now() / 1000) + 360,
   messages: [
     {
-      address: process.env.NEXT_PUBLIC_CONTRACT,
+      address: 'kQAkmmMzTgphBGpvDOrs_o2yKQZwOr0IkPQXNVY1XVKm81TV',
       amount: toNano(0.15).toString(),
       payload: body.toBoc().toString("base64") // payload with comment in body
     }
@@ -47,55 +47,25 @@ export const Mint = () => {
   const wallet = useTonWallet();
   const userFriendlyAddress = useTonAddress();
 
-  useEffect(() => {
-    const storedAddress = localStorage.getItem('userFriendlyAddress');
-    if (storedAddress) {
-      sendAddressToFirebase(storedAddress);
-    }
-  }, []);
-
   const handleTransaction = async () => {
     try {
-      console.log('Attempting to send transaction...');
       await tonConnectUi.sendTransaction(tx);
-      console.log('Transaction sent successfully.');
-
-      const storedAddress = localStorage.getItem('userFriendlyAddress');
-      if (!storedAddress) {
-        localStorage.setItem('userFriendlyAddress', userFriendlyAddress);
-        console.log('Address saved to localStorage.');
-      }
-
-      await addAddressToCooldown(userFriendlyAddress); // Add address to cooldown after successful transaction
-      console.log('Address added to cooldown.');
+      addAddressToCooldown(userFriendlyAddress); // Add address to cooldown after successful transaction
     } catch (e) {
-      console.error('Error sending transaction or adding address to cooldown:', e);
       setError(e.message); // Set the error message
     }
   };
 
   // Function to add address to Firebase
-  const addAddressToCooldown = async (address) => {
-    try {
-      const addressRef = ref(database, `cooldown/${address}`);
-      await set(addressRef, {
-        timestamp: Date.now()
-      });
-    } catch (error) {
+  const addAddressToCooldown = (address) => {
+    const addressRef = ref(database, `cooldown/${address}`);
+    set(addressRef, {
+      timestamp: Date.now()
+    }).then(() => {
+      console.log('Address added to cooldown');
+    }).catch((error) => {
       console.error('Error adding address to cooldown:', error);
-      throw error; // Re-throw the error to be caught in handleTransaction
-    }
-  };
-
-  // Function to send address from localStorage to Firebase
-  const sendAddressToFirebase = async (address) => {
-    try {
-      console.log('Sending address from localStorage to Firebase...');
-      await addAddressToCooldown(address);
-      console.log('Address sent to Firebase.');
-    } catch (error) {
-      console.error('Error sending address from localStorage to Firebase:', error);
-    }
+    });
   };
 
   return (
